fdcc6551a75873f0f17d3548fecd89033da180c8
a.masalov <delsol@bk.ru>
2020-02-22 22:00:46 +0300

    update task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,16 +1,34 @@
 import { Injectable } from '@nestjs/common'
-import * as nanoid from 'nanoid'
+import nanoid from 'nanoid'
 import { Task, TaskStatus } from './tasks.model'
+import { CreateTaskDto } from './dto/create-task.dto'
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = []
 
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasks
   }
 
-  createTask(title: string, description: string) {
+  // FIXME: что-то сделать с undefined
+  getTaskById(id: string): Task | undefined {
+    return this.tasks.find(task => task.id === id)
+  }
+
+  // FIXME: что-то сделать с undefined
+  deleteTask(id: string): Task | undefined {
+    const taskToDelete = this.tasks.find(task => task.id === id)
+
+    if (taskToDelete) {
+      this.tasks = this.tasks.filter(task => task.id !== id)
+    }
+
+    return taskToDelete
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto
     const task: Task = {
       id: nanoid(),
       title,
@@ -19,6 +37,17 @@ export class TasksService {
     }
 
     this.tasks.push(task)
+
     return task
+  }
+
+  updateTask(id: string, status: TaskStatus): Task | undefined {
+    const taskToUpdate = this.getTaskById(id)
+
+    if (taskToUpdate) {
+      taskToUpdate.status = status
+    }
+
+    return taskToUpdate
   }
 }
